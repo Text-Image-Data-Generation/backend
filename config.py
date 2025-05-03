b769797da6b3c258304f28efb40c7d9f0e2abd72
Shashi-Tej-Reddy-Singa-Reddy <shashitejreddysingareddy@gmail.com>
2025-05-03 13:50:23 +0530

    s

--- config.py
+++
@@ -1,8 +0,0 @@
-# Configuration variables
-
-UPLOAD_TEMP_FOLDER = 'uploaded_images_temp' # New temporary folder for uploads
-AUGMENTED_FOLDER = 'augmented_images'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-SECRET_KEY = 'your_very_secret_and_random_key_here' # CHANGE THIS IN PRODUCTION
-# Add frontend origin for CORS
-FRONTEND_URL = 'http://localhost:3000' # Default Create React App URL
